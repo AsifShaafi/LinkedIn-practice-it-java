6318ff8c3cae1da820146751fa0e7d78255a1234
Asif Imtiaz Shaafi <shaafi.20994@gmail.com>
2024-02-14 03:26:01 +0000

    Finish ch 2

---
+++ _02_02/Ticket.java
@@ -0,0 +1,13 @@
+package _02_02;
+
+public class Ticket {
+  // - A field of type String called destination
+  // - A field called price of type double
+  // - A field called isReturn of type boolean
+  private String destination;
+  private double price;
+  private boolean isReturn;
+  
+  public Ticket() {
+  }
+}
